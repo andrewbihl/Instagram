540bdd35a5ba906d50bfba5e198146c16006833d
JanL <janelle.h.writes@gmail.com>
2016-06-18 18:21:59 -0500

    Add subclasses of ProfileView and set constraints on buttons

--- Instagram/ProfileViewController.swift
+++ Instagram/ProfileViewController.swift
@@ -23,44 +23,42 @@ class ProfileViewController: UIViewController {
         listVC = storyboard.instantiateViewControllerWithIdentifier("imageListVC")
         mapVC = storyboard.instantiateViewControllerWithIdentifier("mapVC")
         lastVC = storyboard.instantiateViewControllerWithIdentifier("lastVC")
-        
         gridVC = storyboard.instantiateViewControllerWithIdentifier("gridVC")
         showGrid()
     }
 
     
     func swapContainerChildView (vc: UIViewController?) {
-        //         listVC is an optional, so we cannot grab its view if it's nil (i.e. doesn't exist) -- .addSubview requires a non-optional to be passed in
+        //  listVC is an optional, so we cannot grab its view if it's nil (i.e. doesn't exist) -- .addSubview requires a non-optional to be passed in
         for viewItem in containerView.subviews {
             viewItem.removeFromSuperview()
         }
-        
         if let vc = vc {
             containerView.addSubview(vc.view)
         }
     }
     
     @IBAction func showGrid() {
-        //       Show the grid, remove existing view from containerView, add view from gridViewController to containerView
+        // Show the grid, remove existing view from containerView, add view from gridViewController to containerView
         swapContainerChildView(gridVC)
         print("Grid Button pressed")
     }
     
     @IBAction func showList(sender: AnyObject) {
+        // Show the list, remove existing view from containerView, add view from listViewController to containerView
         swapContainerChildView(listVC)
         print("List Button pressed")
     }
     
     @IBAction func showMap(sender: AnyObject) {
-        //       Show the map, remove existing viewController from containerView, add view from mapViewController to containerView
+        // Show the map, remove existing viewController from containerView, add view from mapViewController to containerView
         swapContainerChildView(mapVC)
         print("Map Button pressed")
     }
     
     @IBAction func showLast(sender: AnyObject) {
-        //       Show the lastView, remove existing viewController from containerView, add view from lastViewController to containerView
+        // Show the lastView, remove existing viewController from containerView, add view from lastViewController to containerView
         swapContainerChildView(lastVC)
-        
         print("Last Button pressed")
     }
     
