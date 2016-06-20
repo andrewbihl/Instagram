11f0ef8b5330fbda6dd0a4840d9272703e1b10cb
JanL <janelle.h.writes@gmail.com>
2016-06-20 11:47:35 -0500

    Set up Profile storyboard, Delete Main.storyboard

--- Instagram/GridCollectionViewController.swift
+++ Instagram/GridCollectionViewController.swift
@@ -18,7 +18,7 @@ class GridCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        samplePhotos = ["1.jpg", "2.jpg", "3.jpg","4.jpg", "5.jpg", "6.jpg", "7.jpg"]
+        samplePhotos = ["1.jpg", "2.jpg", "3.jpg","4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
         collectionView?.reloadData()
     }
     
